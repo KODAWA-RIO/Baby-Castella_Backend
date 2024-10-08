452fa411d95a33c27705da0138b68a8ec2010b1c
yuuya-hoshino <yuuya-hoshino@github.com>
2024-10-08 22:08:29 +0900

    テーブル作成

---
+++ src/database/migrations/2024_10_08_124806_create_merchandise_to_order_table.php
@@ -0,0 +1,25 @@
+<?php
+
+// database/migrations/xxxx_xx_xx_create_merchandise_to_order_table.php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMerchandiseToOrderTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('merchandise_to_order', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('merchandise_id')->constrained('merchandises'); // 外部キー
+            $table->foreignId('order_id')->constrained('orders'); // 外部キー
+            $table->integer('pieces'); // 注文個数
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('merchandise_to_order');
+    }
+}
---
+++ src/database/migrations/2024_10_08_124806_create_topping_to_order_table.php
@@ -0,0 +1,26 @@
+<?php
+
+// database/migrations/xxxx_xx_xx_create_topping_to_order_table.php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateToppingToOrderTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('topping_to_order', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('topping_id')->constrained('toppings'); // 外部キー
+            $table->foreignId('order_id')->constrained('orders'); // 外部キー
+            $table->integer('pieces'); // 注文個数
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('topping_to_order');
+    }
+}
+
