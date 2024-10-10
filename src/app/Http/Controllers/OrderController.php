51b0694c7376cbdb05bb551f13f01bbf7b4e08df
yuuya-hoshino <yuuya-hoshino@github.com>
2024-10-10 17:15:23 +0900

    一覧画面途中

---
+++ src/app/Http/Controllers/OrderController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {  
+        // Orderデータにupdated_atをフォーマットして追加
+        $orders = Order::all()->map(function ($order) {
+            // updated_at を日付のみにフォーマット
+            $order->formatted_date = $order->updated_at->format('Y-m-d');
+            return $order;
+        });
+
+        // 加工したデータを返す
+        return response()->json($orders);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- src/routes/api.php
+++ src/routes/api.php
@@ -3,8 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MerchandiseController;
+use App\Http\Controllers\ToppingController;
+use App\Http\Controllers\OrderController;
 
 Route::get('/merchandises', [MerchandiseController::class, 'index']);
+Route::get('/toppings', [ToppingController::class, 'index']);
+Route::get('/orders', [OrderController::class, 'index']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
