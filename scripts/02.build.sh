7725caa55e212c239840ee79b47ea4d0acf14f71
Kamechan516 <kamechan5062@gmail.com>
2024-10-10 10:10:40 +0900

    🐛 docker-composeの修正

--- scripts/02.build.sh
+++ scripts/02.build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker build --target web-dev -t laravel:web ./Docker && docker build --target app-dev -t laravel:app ./Docker && docker build --target db-dev -t laravel:db ./Docker && docker build --target admin -t laravel:admin ./Docker
+docker build --target web-dev -t laravel:web ./Docker && docker build --target app-dev -t laravel:app ./Docker && docker build --target db-dev -t laravel:db ./Docker && docker build --target admin -t laravel:admin ./Docker \
+&& mkdir -p ./src/vendor
