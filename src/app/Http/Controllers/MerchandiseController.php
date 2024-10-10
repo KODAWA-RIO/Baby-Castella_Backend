f1748627b88f9fccf1ee2359103730b3f05ad7eb
yuuya-hoshino <yuuya-hoshino@github.com>
2024-10-10 11:20:13 +0900

    api作成

---
+++ src/app/Http/Controllers/MerchandiseController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Merchandise;
+use Illuminate\Http\Request;
+
+class MerchandiseController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Merchandise::all());
+    }
+}
