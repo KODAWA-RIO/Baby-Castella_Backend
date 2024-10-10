e6db4557649aaee487464986e30d0d5828bffe85
Kamechan516 <kamechan5062@gmail.com>
2024-10-10 10:01:58 +0900

    🐛 シェルスクリプトの修正と.env.exampleの修正

--- scripts/01.env_copy.sh
+++ scripts/01.env_copy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Copy the environment file to the current directory
-cp ../src/.env.example ../src/.env
+cp ./src/.env.example ./src/.env
 
 if [ $? -eq 0 ]; then
   echo ".envファイルのコピーが完了しました。環境変数の変更を忘れずに行ってください"
