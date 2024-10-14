95fcb0364532f5bc23bde13723322938086cab63
yuuya-hoshino <yuuya-hoshino@github.com>
2024-10-14 22:43:19 +0900

    削除時の在庫

--- src/app/Models/Order.php
+++ src/app/Models/Order.php
@@ -18,5 +18,17 @@ class Order extends Model
     {
         return $this->hasMany(ToppingToOrder::class);
     }
-}
 
+    // 削除イベントに対する処理
+    protected static function booted()
+    {
+        static::deleting(function ($order) {
+            // 関連する商品の在庫を戻す
+            foreach ($order->merchandiseToOrders as $merchandiseToOrder) {
+                $merchandise = $merchandiseToOrder->merchandise;
+                $merchandise->stock += $merchandiseToOrder->pieces;
+                $merchandise->save();
+            }
+        });
+    }
+}
